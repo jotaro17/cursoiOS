aa8d15099d3d66cecd285d21ea71db41699f4165
Alberto Lobo <alberto.lobo@luceit.es>
2012-02-06 10:11:41 +0100

    Initial Commit

---
+++ cursoiOSTests/cursoiOSTests.h
@@ -0,0 +1,13 @@
+//
+//  cursoiOSTests.h
+//  cursoiOSTests
+//
+//  Created by Alberto Lobo Muñoz on 06/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface cursoiOSTests : SenTestCase
+
+@end
---
+++ cursoiOSTests/cursoiOSTests.m
@@ -0,0 +1,32 @@
+//
+//  cursoiOSTests.m
+//  cursoiOSTests
+//
+//  Created by Alberto Lobo Muñoz on 06/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "cursoiOSTests.h"
+
+@implementation cursoiOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in cursoiOSTests");
+}
+
+@end
