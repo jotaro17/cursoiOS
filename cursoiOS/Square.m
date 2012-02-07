aaff00ac28294d0636bdbeffb475e00a1bfcc8ad
Alberto Lobo <alberto.lobo@luceit.es>
2012-02-07 12:03:07 +0100

    Ejercicio 6a: Crear clase Square hereda de Rectangulo

---
+++ cursoiOS/Square.m
@@ -0,0 +1,13 @@
+//
+//  Square.m
+//  cursoiOS
+//
+//  Created by Alberto Lobo Muñoz on 07/02/12.
+//  Copyright (c) 2012 Alberto Lobo Inc. All rights reserved.
+//
+
+#import "Square.h"
+
+@implementation Square
+
+@end
